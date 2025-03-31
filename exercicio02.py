a8d92d8a79bb88ee63f09442ea456db0c9ac51ed
Adryan Lima <acals@poli.br>
2025-03-31 14:55:00 -0300

    adicionei Exercicio 2

---
+++ exercicio02.py
@@ -0,0 +1,11 @@
+num1 = float(input("Digite o  primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+soma = num2+num1
+subtracao = num1-num2
+multiplicacao = num1*num2
+divisao = num1/num2
+
+print(f"A soma é: {soma}")
+print(f"A subtração é: {subtracao}")
+print(f"A multiplicação é: {multiplicacao}")
+print(f"A divisão é: {divisao}")
