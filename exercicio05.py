b119acc63518a6a9453bc32ddcb98aaf45cadcfe
Adryan Lima <acals@poli.br>
2025-03-31 16:52:06 -0300

    formatei os numeros decimais do codigo

--- exercicio05.py
+++ exercicio05.py
@@ -3,8 +3,8 @@ nota2 = float(input("Digite a nota 2 do aluno: "))
 nota3 = float(input("Digite a nota 3 do aluno: "))
 media = (nota3+nota2+nota1)/3
 if media >= 7:
-    print(f"Aluno aprovado com media {media}")
+    print(f"Aluno aprovado com media {media:.2f}")
 if media < 4:
-    print(f"Aluno reprovado com media {media}")
+    print(f"Aluno reprovado com media {media:.2f}")
 else:
-    print(f"Aluno em recuperação com media {media}")
+    print(f"Aluno em recuperação com media {media:.2f}")
