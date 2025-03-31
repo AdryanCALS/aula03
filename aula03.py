26eb3b55e952792a42f69a38cf9054d4680d1808
Adryan Lima <acals@poli.br>
2025-03-31 14:19:54 -0300

    Segunda parte do exercicio

--- aula03.py
+++ aula03.py
@@ -2,5 +2,8 @@
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 salario = float(input("Digite o seu salario: "))
+aumento = float(input("Digite a porcentagem do seu aumento: "))
+salarioaumento = salario*(aumento/100)+salario
 print(f"Ola {nome}, voce tem {idade} anos e seu salario é de: {salario}")
+print(f"Seu salario apos o aumento é de: {salarioaumento}")
 
