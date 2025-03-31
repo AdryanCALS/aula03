0735092fb94e5aa2db72e603e7e8055504eac923
Adryan Lima <acals@poli.br>
2025-03-31 14:15:43 -0300

    primeira parte do exercicio

--- aula03.py
+++ aula03.py
@@ -1,4 +1,6 @@
+#Exercicio 1
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 salario = float(input("Digite o seu salario: "))
 print(f"Ola {nome}, voce tem {idade} anos e seu salario é de: {salario}")
+
