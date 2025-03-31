cbd23d92ad46df246cc3c91132f0f087117fb4c5
Adryan Lima <acals@poli.br>
2025-03-31 15:46:48 -0300

    exercicio 3

---
+++ exercicio03.py
@@ -0,0 +1,10 @@
+a = int(input("Digite o valor da primeira variavel: "))
+b = int(input("Digite o valor da segunda variavel: "))
+x = 0
+print(f"O valor de A é: {a}")
+print(f"O valor de B é: {b}")
+x = a
+a = b
+b = x
+print(f"O valor de A é: {a}")
+print(f"O valor de B é: {b}")
